de68fccb9bf4ed301d0341105f1e5fe0cb1e1ea9
Arnold van der Drift <arnold.vander.drift@valtech.nl>
2019-02-11 11:39:32 +0100

    TC: Initial commit

---
+++ TestCore/Services/ITvMazeScraperService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCore.Services
+{
+    public interface ITvMazeScraperService
+    {
+        void ScrapeShowInformation();
+
+        void ScrapeCastInformation();
+    }
+}
---
+++ TestCore/Services/TvMazeScraperService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestCore.Services
+{
+    public class TvMazeScraperService : ITvMazeScraperService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TvMazeScraperService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public void ScrapeShowInformation()
+        {
+
+        }
+
+        public void ScrapeCastInformation()
+        {
+
+        }
+    }
+}
